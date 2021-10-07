87ff4a78908a069b5c44702a5a70c2f79c8a928a
Leo Spratt <enchant97@users.noreply.github.com>
2021-10-07 21:31:36 +0100

    add configs

--- .bashrc
+++ .bashrc
@@ -23,3 +23,10 @@ alias ....='cd ../../..'
 alias c='clear'
 alias cls='clear'
 alias neofetch='fastfetch'
+alias htop="btop"
+alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
+alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first --icons'  # long format
+alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
+alias cat='bat --style header --style rules --style snip --style changes --style header'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,3 +18,10 @@ abbr -a .... 'cd ../../..'
 abbr -a c 'clear'
 abbr -a cls 'clear'
 abbr -a neofetch 'fastfetch'
+abbr -a htop 'btop'
+abbr -a ls 'exa -al --color=always --group-directories-first --icons' # preferred listing
+abbr -a la 'exa -a --color=always --group-directories-first --icons'  # all files and dirs
+abbr -a ll 'exa -l --color=always --group-directories-first --icons'  # long format
+abbr -a lt 'exa -aT --color=always --group-directories-first --icons' # tree listing
+abbr -a l. "exa -a | egrep '^\.'"                                     # show only dotfiles
+abbr -a cat 'bat --style header --style rules --style snip --style changes --style header'
